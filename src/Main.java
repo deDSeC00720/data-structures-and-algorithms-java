9b1cfa93f69475499d80f429a9ef2da28a8b6ee1
deDSeC00720 <terminator.karansharma@gmail.com>
2022-10-10 02:45:35 +0530

    add selection sort

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import algorithms.sorting.Sort;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner;
+
+    static {
+        scanner = new Scanner(System.in);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            arrayList.add(scanner.nextInt());
+        }
+        System.out.println("Before sorting:");
+        System.out.println(arrayList);
+
+        Sort.selectionSort(arrayList);
+
+        System.out.println("After sorting:");
+        System.out.println(arrayList);
     }
 }
---
+++ src/algorithms/sorting/Sort.java
@@ -0,0 +1,19 @@
+package algorithms.sorting;
+
+import java.util.List;
+
+public class Sort {
+    public static void selectionSort(List<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(minIndex) > list.get(j)) {
+                    minIndex = j;
+                }
+            }
+            int temp = list.get(i);
+            list.set(i, list.get(minIndex));
+            list.set(minIndex, temp);
+        }
+    }
+}
